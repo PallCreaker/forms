5346c5d1454bfcc71ba4b5e73d24120d20be5762
kazumasa.fujita <kazumasa.fuji@gmail.com>
2017-10-08 07:14:47 +0900

    comp view

--- forms/urls.py
+++ forms/urls.py
@@ -4,5 +4,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /froms/ name="function name of views"
     url(r'^$', views.index, name='index'),
+    # ex: /forms/5/ question_id = 引数 of views
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /forms/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /forms/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+    # not recommended
+    # url(r'^polls/latest\.html$', views.index),
 ]
